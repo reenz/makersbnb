e1271886beca04c2180d8228163fc13de2018dd8
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-02-08 10:30:23 +0000

    updated data properties for space

--- app/models/space.rb
+++ app/models/space.rb
@@ -4,10 +4,10 @@ class Space
   belongs_to :user
 
   property :id, Serial
-  property :space_name, String
-  property :space_description, Text
-  property :space_price, Integer
-  property :availability_start, Date
-  property :availability_end, Date
+  property :space_name, String, required: true
+  property :space_description, Text, required: true
+  property :space_price, String, required: true
+  property :availability_start, String, required: true
+  property :availability_end, String, required: true
 
 end
